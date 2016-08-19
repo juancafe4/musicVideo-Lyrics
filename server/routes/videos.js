47ddcca9d7a4b60a181051a3a90d81d945eb0314
Juan Carlos <juancafe2@gmail.com>
2016-08-19 15:37:15 -0700

    Project set up

---
+++ server/routes/videos.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const Video = require('../models/video');
+
+router.route('/')
+  .get((req, res) => {
+    Todo.find({}, (err, todos) => {
+      res.send('works!')
+    });
+  })
+
+module.exports = router
