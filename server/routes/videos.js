22eecce2d6d9314a423975d5b7373b6e81086a47
Seneca Sisneros <seneca.sisneros@yahoo.com>
2016-08-19 18:04:05 -0700

    routes done and getAll API.js

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express');
 const router = express.Router();
 
@@ -5,9 +7,38 @@ const Video = require('../models/video');
 
 router.route('/')
   .get((req, res) => {
-    Todo.find({}, (err, todos) => {
-      res.send('works!')
+    Video.find({}, (err, videos) => {
+      res.status(err ? 400 : 200).send(err || videos);
     });
   })
+.post((req, res) => {
+  Video.create(req.body, (err, videos) => {
+    res.status(err ? 400 : 200).send(err || videos);
+  })
+})
+
+router.route('/:id')
+.get((req, res) => {
+  Video.findById(req.params.id, (err, video) => {
+    res.status(err ? 400 : 200).send(err || video);
+  });
+})
+.delete((req, res) => {
+  Video.findByIdAndRemove(req.params.id, (err, videos) => {
+    res.status(err ? 400 : 200).send(err || videos);
+  })
+})
+.put((req, res) => {
+  Video.findByIdAndUpdate(req.params.id, {$set: req.body}, {new: true}, (err, videos) => {
+    if(err) {
+      return res.status(400).send(err);
+    }
+
+    Video.find({}, (err, videos) => {
+      res.status(err ? 400 : 200).send(err || videos);
+    });
+  });
+})
+
 
 module.exports = router;
